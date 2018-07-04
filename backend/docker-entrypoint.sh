26da5f911a57c0bd18a688da6462e7b9889d4ef7
Michał Piotr Stankiewicz <--local>
2018-07-04 10:17:38 +0200

    Semifinal

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -4,5 +4,5 @@ echo "Apply database migrations"
 python3 manage.py migrate --settings=regalix.settings.docker
 
 echo "Start server"
-python3 manage.py runserver 0.0.0.0:8000 --settings=regalix.settings.docker
+gunicorn --env DJANGO_SETTINGS_MODULE=regalix.settings regalix.wsgi
 
--- frontend/src/components/Root.jsx
+++ frontend/src/components/Root.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Provider} from 'react-redux';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import configureStore from '../store/configureStore';
 import About from './About';
 import Contact from './Contact';
